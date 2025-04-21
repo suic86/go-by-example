9a8a794224a756533be1def1101631a32a7893fa
suic86 <marko.gabriel.cz@gmail.com>
2025-04-21 20:20:54 +0200

    embed directive example

---
+++ embed-directive/embed-directive.go
@@ -0,0 +1,25 @@
+package main
+
+// if you don't use any exported identifiers from this package,
+// you can do a black import with `_ "embed"`.
+import "embed"
+
+//go:embed folder/single_file.txt
+var fileString string
+
+//go:embed folder/single_file.txt
+var fileByte []byte
+
+//go:embed folder/single_file.txt
+//go:embed folder/*hash
+var folder embed.FS
+
+func main() {
+	print(fileString)
+	print(string(fileByte))
+
+	content1, _ := folder.ReadFile("folder/file1.hash")
+	print(string(content1))
+	content2, _ := folder.ReadFile("folder/file2.hash")
+	print(string(content2))
+}
---
+++ embed-directive/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p folder
+
+### create the files to embed
+echo "hello go" > folder/single_file.txt
+echo "123" > folder/file1.hash
+echo "456" > folder/file2.hash
+
+# don't put the files in folder to git
+echo "*" > folder/.gitignore
+
+go run embed-directive.go
